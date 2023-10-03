809f3e3f379adb0db8d774bd91507e2042da41fe
Lucifer <38715155+jonesroot@users.noreply.github.com>
2023-10-04 00:55:40 +0700

    bradst.py

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -41,15 +41,14 @@ async def gcast(event):
         msg = await event.get_reply_message()
     else:
         return await eor(
-            event,("**`Kiw Jomblo..`")
+            event.eor("**`Kiw Jomblo..`")
         )
-    kk = await event.eor("`LagiNgirimBacotanAlaymu")
+    kk = await event.eor("`LagiNgirimBacotanAlaymu`")
     er = 0
     done = 0
     err = ""
     chat_blacklist = udB.get_key("GBLACKLISTS") or []
     chat_blacklist.append(-1001608847572)
-    chat_blacklist.append(-1001905557194)
     udB.set_key("GBLACKLISTS", chat_blacklist)
     async for x in event.client.iter_dialogs():
         if x.is_group:

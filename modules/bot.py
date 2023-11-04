2c26010338b14a12137c943111d1be4874413237
KojiraReyyAnata <113760925+KojiraReyyAnata@users.noreply.github.com>
2023-11-05 03:27:53 +0700

    update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -192,7 +192,7 @@ async def _(event):
     x = await event.eor("Ping !")
     end = round((time.time() - start) * 1000)
     uptime = time_formatter((time.time() - start_time) * 1000)
-    await x.edit(f"**Nyaut Key 📞** - `{end}ms`\n**Time 🆓** - `{uptime}`")
+    await x.edit(f"**Nyepong 😍** - `{end}ms`\n**MASA AKTIF ☺** - `{uptime}`")
 
 
 async def get_readable_time(seconds: int) -> str:

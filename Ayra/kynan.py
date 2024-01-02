fd607aff6d33aebed777eee5fc228e08788d406f
Keysmith <113760925+KojiraReyyAnata@users.noreply.github.com>
2024-01-02 22:21:35 +0700

    update kynan.py

--- Ayra/kynan.py
+++ Ayra/kynan.py
@@ -180,7 +180,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**✘ AYRA-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/skyfand)"
+                    link = "[Group Support](https://t.me/keysupport1)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
